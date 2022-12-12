70c8412d187c3c93edfcaf02cb245913f29b09df
lsm1987 <1987lsm@gmail.com>
2022-12-12 18:00:21 +0900

    [MSW/Live2D-WebView] 웹뷰 출력용 페이지 추가

---
+++ msw/live2d-webview-sample/scripts/main.js
@@ -0,0 +1,68 @@
+const backgroundPath = "images/background.jpeg";
+const modelPath = "models/shizuku/shizuku.model.json";
+
+(async function main() {
+  const app = new PIXI.Application({
+    view: document.getElementById("canvas"),
+    autoStart: true,
+    resizeTo: window,
+    backgroundColor: 0xffffff
+  });
+
+  let backgroundSprite;
+  let backgroundSpriteOriginalHeight;
+  let model;
+  let modelOriginalHeight;
+
+  app.loader
+    .add(backgroundPath)
+    .load(setup);
+
+  async function setup() {
+    const screenWidth = app.screen.width;
+    const screenHeight = app.screen.height;
+    console.log("screen w:" + screenWidth + " h: " + screenHeight);
+
+    backgroundSprite = new PIXI.Sprite(app.loader.resources[backgroundPath].texture);
+    //console.log("background w:" + backgroundSprite.width + " h: " + backgroundSprite.height);
+    backgroundSpriteOriginalHeight = backgroundSprite.height;
+
+    app.stage.addChild(backgroundSprite);
+    backgroundSprite.anchor.set(0.5, 0.5);
+    ArrangeBackgroundTransform(backgroundSprite, backgroundSpriteOriginalHeight, screenWidth, screenHeight);
+
+    model = await PIXI.live2d.Live2DModel.from(modelPath, { autoInteract: false });
+    //console.log("model w:" + model.width + " h: " + model.height); // model w:1280 h: 1380
+    modelOriginalHeight = model.height;
+    
+    app.stage.addChild(model);
+    model.anchor.set(0.5, 1);
+    ArrangeModelTransform(model, modelOriginalHeight, screenWidth, screenHeight);
+  }
+
+  app.renderer.on('resize', (width, height) => {
+    if (backgroundSprite && backgroundSpriteOriginalHeight)
+    {
+      ArrangeBackgroundTransform(backgroundSprite, backgroundSpriteOriginalHeight, width, height);
+    }
+
+    if (model && modelOriginalHeight)
+    {
+      ArrangeModelTransform(model, modelOriginalHeight, width, height);
+    }
+  })
+})();
+
+function ArrangeBackgroundTransform(backgroundSprite, originalHeight, screenWidth, screenHeight) {
+  var scale = screenHeight / originalHeight;
+  backgroundSprite.scale.set(scale);
+  backgroundSprite.x = screenWidth / 2.0;
+  backgroundSprite.y = screenHeight / 2.0;
+}
+
+function ArrangeModelTransform(model, originalHeight, screenWidth, screenHeight) {
+  var scale = screenHeight / originalHeight;
+  model.scale.set(scale);
+  model.x = screenWidth / 2.0;
+  model.y = screenHeight;
+}
