13b7e61b67abf81e30ededd579e64dd6bcdebdf2
lsm1987 <1987lsm@gmail.com>
2022-12-12 23:30:45 +0900

    [MSW/Live2D-WebView] 모델 로딩중 표시 추가

--- msw/live2d-webview-sample/scripts/main.js
+++ msw/live2d-webview-sample/scripts/main.js
@@ -23,6 +23,7 @@ const modelPath = "models/shizuku/shizuku.model.json";
     const screenHeight = app.screen.height;
     console.log("screen w:" + screenWidth + " h: " + screenHeight);
 
+    // 배경
     backgroundSprite = new PIXI.Sprite(app.loader.resources[backgroundPath].texture);
     //console.log("background w:" + backgroundSprite.width + " h: " + backgroundSprite.height);
     backgroundSpriteOriginalHeight = backgroundSprite.height;
@@ -31,6 +32,26 @@ const modelPath = "models/shizuku/shizuku.model.json";
     backgroundSprite.anchor.set(0.5, 0.5);
     ArrangeBackgroundTransform(backgroundSprite, backgroundSpriteOriginalHeight, screenWidth, screenHeight);
 
+    // 로딩 표시
+    const loadingText = new PIXI.Text('Loading...', {
+      fontSize: 75,
+      fill: "black"
+    });
+
+    const loadingSprite = new PIXI.Sprite(PIXI.Texture.WHITE);
+    loadingSprite.width = loadingText.width;
+    loadingSprite.height = loadingText.height;
+
+    const loadingContainer = new PIXI.Container();
+    loadingContainer.addChild(loadingSprite, loadingText);
+    app.stage.addChild(loadingContainer);
+
+    loadingContainer.pivot.x = loadingContainer.width;
+    loadingContainer.pivot.y = loadingContainer.height;
+    loadingContainer.x = screenWidth;
+    loadingContainer.y = screenHeight;
+
+    // 모델
     model = await PIXI.live2d.Live2DModel.from(modelPath, { autoInteract: false });
     //console.log("model w:" + model.width + " h: " + model.height); // model w:1280 h: 1380
     modelOriginalHeight = model.height;
@@ -38,6 +59,9 @@ const modelPath = "models/shizuku/shizuku.model.json";
     app.stage.addChild(model);
     model.anchor.set(0.5, 1);
     ArrangeModelTransform(model, modelOriginalHeight, screenWidth, screenHeight);
+
+    // 로딩 해제
+    app.stage.removeChild(loadingContainer);
   }
 
   app.renderer.on('resize', (width, height) => {
